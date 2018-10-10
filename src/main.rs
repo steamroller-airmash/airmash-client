cb299fb8f60b71dca6dab7a7c72190c47ce61b5c
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-10-10 01:51:33 -0400

    Various updates

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@ extern crate log;
 extern crate simple_logger;
 
 use airmash_client::Client;
-use airmash_client::protocol::{Position, KeyCode};
+//use airmash_client::protocol::{Position, KeyCode};
 
 use std::error::Error;
 use std::time::Duration;
 
 fn run_bot(name: &str, server: &str) -> Result<(), Box<Error>> {
     Client::new(server)?
-        .login(name, "JOLLY")?
-        .wait(Duration::from_secs(3600*24))?
+        .login(name, "CA")?
+        .wait(Duration::from_secs(3600))?
         .disconnect()?;
 
     Ok(())
 }
 
 //const SERVER: &'static str = "wss://game.airmash.steamroller.tk/dev";
-const SERVER: &'static str = "ws://localhost:3501";
+const SERVER: &'static str = "wss://game-us-s1.airma.sh/ctf1";//"ws://localhost:3501";
 
 fn main() {
     simple_logger::init_with_level(log::Level::Info).unwrap();
